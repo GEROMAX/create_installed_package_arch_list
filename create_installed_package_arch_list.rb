7e89354c086e1a40b50c127664e44a03a92851e5
GEROMAX <tobita0112@gmail.com>
2017-12-22 12:59:32 +0900

    add func

--- create_installed_package_arch_list.rb
+++ create_installed_package_arch_list.rb
@@ -1,19 +1,30 @@
 #インストールパッケージの名前とアーキテクチャのリスト
 def create_installed_package_arch_list
-    # exec yum
-    package_infos = `yum list installed`
+  # exec yum
+  package_infos = `yum list installed`
 
-    archlist = Hash.new()
-    skiping = true
-    package_infos.split(/\n/).each { | line |
-        if skiping
-            skiping = !/Installed Packages/.match(line)
-            next
-        end
-        name_arch = line.split(' ')[0].split('.')
-        archlist[name_arch[0]] = name_arch[1]
-    }
-    archlist
+  archlist = Hash.new()
+  skiping = true
+  package_infos.split(/\n/).each do | line |
+    if skiping
+      skiping = !/Installed Packages/.match(line)
+      next
+    end
+    name_arch = line.split(' ')[0].split('.')
+    archlist[name_arch[0]] = name_arch[1]
+  end
+  archlist
+end
+#インストール済みパッケージの名前、アーキテクチャ、エポック取得
+def get_installed_package_arch_epoch
+  details = Hash.new
+  #yumよりrepoqueryのほうが成形しやすい
+  command_results = `repoquery --all --pkgnarrow=installed --qf="%{name}.%{arch}.%{epoch}"`
+  command_results.split(/\n/).each do |line|
+    detail = line.split('.')
+    details[detail[0]] = detail
+  end
+  details
 end
 
-puts create_installed_package_arch_list
+puts get_installed_package_arch_epoch
